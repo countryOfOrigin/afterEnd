b455a11e1ddd4989293b60aded687a11795b6e1a
qf <legend9308@163.com>
2017-08-07 12:08:09 +0800

    ci----insert_user/login_user

---
+++ application/controllers/User.php
@@ -0,0 +1,43 @@
+<?php  
+	defined('BASEPATH') OR exit('No direct script access allowed');
+	class User extends CI_Controller{
+		public function __construct(){
+			parent::__construct();
+			$this->load->model('user_model');
+		}
+		public function insert_user(){
+			$uname=$this->input->post('name');
+			$num=$this->input->post('tel');
+			$psw=$this->input->post('psw');
+			$result=$this->user_model->check_num($num);
+			if($result){
+				echo "6";
+			}else{
+				$result=$this->user_model->insert_user($uname,$num,$psw);
+				if($result){
+					echo "7";
+				}else{
+					echo "8";
+				}
+			}
+		}
+		public function login_user(){
+			$num=$this->input->post('tel');
+			$psw=$this->input->post('psw');
+			$result=$this->user_model->login_user($num,$psw);
+			if($result){
+				$result=json_encode($result);
+				echo $result;
+			}else{
+				echo "3";
+			}
+		}
+		// public function get_user(){
+
+
+		// 	$result=$this->user_model->get_user($uid);
+	 }
+
+
+
+?>
---
+++ application/models/User_model.php
@@ -0,0 +1,36 @@
+<?php  
+	defined('BASEPATH') OR exit('No direct script access allowed');
+	class User_model extends CI_Model{
+		// public function check_num($num){
+		// 	$this->db->from('users');
+		// 	$this->db->where('telephone',$num);
+		// 	$query=$this->db->get();
+		// 	return $query->row();
+		// }
+		public function insert_user($uname,$num,$psw){
+			$arr=array(
+				'name'=>$uname,
+				'pass'=>$pwd,
+				'telephone'=>$num,
+				);
+			$query=$this->db->insert('users',$arr);
+			return $query;
+		}
+		public function login_user($num,$psw){
+			$arr=array(
+				'telephone'=>$num,
+				'pass'=>$psw
+				);
+			$this->db->select('user_id');
+			$this->db->select('name');
+			$this->db->from('users');
+			$this->db->where($arr);
+			$query=$this->db->get();
+			return $query->row();
+		}
+		public function get_user(){
+
+		}
+	}
+
+?>
