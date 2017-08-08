1e1da846fc83e1c2d3c0fcbc33ba30e7cb8b77e8
qf <legend9308@163.com>
2017-08-08 10:02:29 +0800

    商品分页

--- application/controllers/Goods.php
+++ application/controllers/Goods.php
@@ -8,10 +8,27 @@
 		public function get_goods(){
 			$arr=[];
 			for ($i=0; $i < 8; $i++) { 
-				$result=$this->goods_model->get_goods($i);
+				$result=$this->goods_model->get_goods($i,4,null);
 				array_push($arr,$result);
 			}
 			echo json_encode($arr);
+		}
+		public function paging(){
+			$cls=$this->input->get('cls');
+			$count=$this->input->get('count');
+			$page=$this->input->get('page');
+
+			if($cls){
+				$result=$this->goods_model->get_goods($cls,$count,($page-1)*$count);
+				echo json_encode($result);
+			}else{
+				$arr=[];
+				for ($i=0; $i < 8; $i++) { 
+					$result=$this->goods_model->get_goods($i,$count,null,($page-1)*$count);
+					array_push($arr,$result);
+				}
+				echo json_encode($arr);
+			}
 		}
 	}
 
